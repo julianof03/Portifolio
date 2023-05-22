0eee4efa740f2889418befd77f29a6ebd99cb3d8
julianof03 <jlprod08@gmail.com>
2023-05-22 03:31:55 -0300

    ProjectsRework

---
+++ src/pages/Projects.js
@@ -0,0 +1,49 @@
+import BatePapoUol from "./images/BatePapoUol.png"
+import Drivent from "./images/Drivent.png"
+import DriventEats from "./images/DriventEats.png"
+import Efood from "./images/E-food.png"
+import Grape from "./images/Grape.png"
+import Linkr from "./images/Linkr.png"
+import myWallet from "./images/myWallet.png"
+import Trackit from "./images/Trackit.png"
+import ZapRecall from "./images/ZapRecall.png"
+
+export const ProjectList = [
+    {
+
+      ProjectName: 'Drivent',
+      Image: Drivent,
+    },
+    {
+      ProjectName: 'DriventEats',
+      Image: DriventEats,
+    },
+    {
+      ProjectName: 'Efood',
+      Image: Efood,
+    },
+    {
+      ProjectName: 'Grape',
+      Image: Grape,
+    },
+    {
+      ProjectName: 'Linkr',
+      Image: Linkr,
+    },
+    {
+      ProjectName: 'BatePapoUol',
+      Image: BatePapoUol,
+    },
+    {
+      ProjectName: 'myWallet',
+      Image: myWallet,
+    },
+    {
+      ProjectName: 'Trackit',
+      Image: Trackit,
+    },
+    {
+      ProjectName: 'ZapRecall',
+      Image: ZapRecall,
+    },
+];
---
+++ src/pages/Tecnologie.js
@@ -0,0 +1,84 @@
+import HtmlImage from "./images/Tecnologies/HTML.png"
+import CssImage from "./images/Tecnologies/Css.png"
+import MongoDbImage from "./images/Tecnologies/MongoDB.png"
+import PostGressImage from "./images/Tecnologies/Postgress.png"
+import ReactImage from "./images/Tecnologies/React.png"
+import SQLImage from "./images/Tecnologies/SQL.png"
+import TypeScriptImage from "./images/Tecnologies/TypeScript.png"
+import javaScriptImage from "./images/Tecnologies/javaScript.png"
+export const TecnologiesList = [
+    {
+      Tecnologie: 'Html',
+      Image: HtmlImage,
+    },
+    {
+      Tecnologie: 'CSS',
+      Image: CssImage,
+    },
+    {
+      Tecnologie: 'JS',
+      Image: javaScriptImage,
+    },
+    {
+      Tecnologie: 'React',
+      Image: ReactImage,
+    },
+    {
+      Tecnologie: 'TypeScrip',
+      Image: TypeScriptImage,
+    },
+    {
+      Tecnologie: 'SQL',
+      Image: SQLImage,
+    },
+    // {
+    //   Tecnologie: 'PostgressSQL',
+    //   Image: PostGressImage,
+    // },
+    // {
+    //   Tecnologie: 'MongoDB',
+    //   Image: MongoDbImage,
+    // },
+    // {
+    //   Tecnologie: 'Jest',
+    // },
+    // {
+    //   Tecnologie: 'Deploy',
+    // },
+    // {
+    //   Tecnologie: 'Prisma',
+    // },
+    // { 
+    //   Tecnologie: 'Api',
+    // },
+    // {
+    //   Tecnologie: 'Git',
+    // },
+    // {
+    //   Tecnologie: 'GitHub',
+    // },
+    // {
+    //   Tecnologie: 'ES6',
+    // },
+    // {
+    //   Tecnologie: 'Express',
+    // },
+    // {
+    //   Tecnologie: 'Node.js',
+    // },
+    // {
+    //   Tecnologie: 'Documentação',
+    // },
+    // {
+    //   Tecnologie: 'Clean Code',
+    // },
+    // {
+    //   Tecnologie: 'Scrum',
+    // },
+    // {
+    //   Tecnologie: 'Trello',
+    // },
+    // {
+    //   Tecnologie: 'Figma',
+    // },
+];
